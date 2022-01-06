36cd301136c37d12389e73480ac9c94904a303b8
Tan-DESKPC <tan@tanho.ca>
2022-01-06 08:56:31 -0500

    add .data pronoun

--- R/utils.R
+++ R/utils.R
@@ -1,18 +1,16 @@
 #### UTILS ###
 # External functions imported and sometimes re-exported
-
 #' Pull `nflreadr` rosters
-#'
 #' @param seasons choose season or range of seasons
 #' @export
 
 .get_rosters <- function(seasons){
   nflreadr::load_rosters(seasons) %>%
-    dplyr::transmute(season,
-                     gsis_id,
-                     full_name,
-                     position = dplyr::if_else(position %in% c("HB","FB"), "RB", position),
-                     birth_date)
+    dplyr::transmute(.data$season,
+                     .data$gsis_id,
+                     .data$full_name,
+                     position = dplyr::if_else(.data$position %in% c("HB","FB"), "RB", .data$position),
+                     .data$birth_date)
 }
 
 #' Tidy eval helpers
@@ -27,8 +25,8 @@
 #' @md
 #' @name tidyeval
 #' @keywords internal
-#' @importFrom rlang .data := .env
-#' @aliases .data := .env
+#' @importFrom rlang .data .env
+#' @aliases .data .env
 NULL
 
 #' Pipe operator
