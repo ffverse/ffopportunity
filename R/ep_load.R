ed70ec4ee317941c4be8ff73348af07681dbab8d
Tan-DESKPC <tan@tanho.ca>
2022-01-11 08:07:00 -0500

    simplify ep_load to one function with args

--- R/ep_load.R
+++ R/ep_load.R
@@ -1,24 +1,30 @@
-#' Load EP Pass Play By Play
+#' Load Expected Points data
 #'
-#' @description Loads multiple seasons from the
-#' [ffexpectedpoints data repository](https://github.com/ffverse/ffexpectedpoints-data)
+#' This function downloads precomputed expected points data from the ffexpectedpoints automated releases.
 #'
-#' @param season A numeric vector of 4-digit years associated with given NFL seasons - defaults to latest season. If set to `TRUE`, returns all available data since 1999.
-#' @param version EP model version: one of "latest" or "v1.0.0" (these are identical at the moment)
+#' @param season A numeric vector of four digit years associated with given NFL seasons - defaults to latest season.
+#' @param type Data type - one of `"weekly"`, `"pbp_pass"`, `"pbp_rush"`, or `"all"`
+#' @param version EP model version: one of "latest" (default) or "v1.0.0" - these are currently identical.
 #'
-#' @return The complete ffexpectedpoints dataset as returned by `ffexpectedpoints::build_nflfastR_pbp()`
-#' (see below) for all given `seasons`
+#' @return a dataframe identical to what would be returned by `ffexpectedpoints::ep_build()` for a given season.
 #'
 #' @examples
 #' \donttest{
-#'   try({ep_load_pbp_pass(2019:2020)})
+#' try({
+#'   ep_load() %>% head(10)
+#'   ep_load(2020:2021) %>% head(10)
+#'   ep_load(2021, type = "pbp_pass") %>% head(10)
+#'   ep_load(2006, type = "pbp_rush", version = "v1.0.0") %>% head(10)
+#' })
 #' }
 #'
 #' @export
-ep_load_pbp_pass <- function(season = nflreadr:::most_recent_season(), version = c("latest","v1.0.0")) {
+ep_load <- function(season = nflreadr:::most_recent_season(),
+                    type = c("weekly","pbp_pass","pbp_rush"),
+                    version = c("latest","v1.0.0")){
 
   version <- rlang::arg_match0(version, c("latest","v1.0.0"))
-
+  type <- rlang::arg_match0(type, c("weekly","pbp_pass","pbp_rush"))
   stopifnot(
     length(season) > 0,
     is.numeric(season),
@@ -28,95 +34,27 @@ ep_load_pbp_pass <- function(season = nflreadr:::most_recent_season(), version =
 
   urls <- paste0(
     "https://github.com/ffverse/ffexpectedpoints/releases/download/",
-    paste0(version,"-data/"),
-    "ep_pbp_pass_",
-    season,
-    ".rds")
+    paste0(version,"-data/ep_"), type, "_", season, ".rds")
 
   p <- NULL
   if (is_installed("progressr")) p <- progressr::progressor(along = season)
   out <- purrr::map_dfr(urls, nflreadr::progressively(rds_from_url, p))
-  out
+  ts <- nflreadr::raw_from_url("https://github.com/ffverse/ffexpectedpoints/releases/download/latest-data/timestamp.txt") %>%
+    rawToChar() %>%
+    as.POSIXct()
+
+  class(out) <- c("ffep_download","tbl_df", "tbl", "data.frame")
+  attr(out, "ep_version") <- version
+  attr(out, "ep_timestamp") <- ts
+  attr(out, "ep_type") <- type
+  return(out)
 }
 
-#' Load EP Rush Play By Play
-#'
-#' @description Loads multiple seasons from the
-#' [ffexpectedpoints data repository](https://github.com/ffverse/ffexpectedpoints-data)
-#'
-#' @param season A numeric vector of 4-digit years associated with given NFL seasons - defaults to latest season. If set to `TRUE`, returns all available data since 1999.
-#' @param version EP model version: one of "latest" or "v1.0.0" (these are identical at the moment)
-#'
-#' @return The complete ffexpectedpoints dataset as returned by `ffexpectedpoints::build_nflfastR_pbp()`
-#' (see below) for all given `seasons`
-#'
-#' @examples
-#' \donttest{
-#'   try({ep_load_pbp_rush(2019:2020)})
-#' }
-#'
 #' @export
-ep_load_pbp_rush <- function(season = nflreadr:::most_recent_season(),
-                             version = "v1.0.0") {
-
-  version <- rlang::arg_match0(version, c("latest","v1.0.0"))
-
-  stopifnot(
-    length(season) > 0,
-    is.numeric(season),
-    all(!is.na(season)),
-    min(season) >= 2006
-  )
-
-  urls <- paste0(
-    "https://github.com/ffverse/ffexpectedpoints/releases/download/",
-    paste0(version,"-data/"),
-    "ep_pbp_rush_",
-    season,
-    ".rds")
-
-  p <- NULL
-  if (is_installed("progressr")) p <- progressr::progressor(along = season)
-  out <- purrr::map_dfr(urls, nflreadr::progressively(rds_from_url, p))
-  out
-}
-
-#' Load Player Level Weekly EP Stats
-#'
-#' @param season A numeric vector of 4-digit years associated with given NFL
-#' seasons - defaults to latest season. If set to `TRUE`,
-#' returns all available data since 1999.
-#' @param version EP model version: one of "latest" or "v1.0.0" (these are identical at the moment)
-#'
-#' @examples
-#' \donttest{
-#'   try({ep_load_player_stats()})
-#' }
-#'
-#' @return A tibble of week-level player stats with corresponding EP variables
-#'
-#' @export
-ep_load_player_stats <- function(season = nflreadr:::most_recent_season(),
-                                 version = c("latest","v1.0.0")) {
-
-  version <- rlang::arg_match0(version, c("latest","v1.0.0"))
-
-  stopifnot(
-    length(season) > 0,
-    is.numeric(season),
-    all(!is.na(season)),
-    min(season) >= 2006
-  )
-
-  urls <- paste0(
-    "https://github.com/ffverse/ffexpectedpoints/releases/download/",
-    paste0(version,"-data/"),
-    "ep_weekly_",
-    season,
-    ".rds")
-
-  p <- NULL
-  if (is_installed("progressr")) p <- progressr::progressor(along = season)
-  out <- purrr::map_dfr(urls, nflreadr::progressively(rds_from_url, p))
-  out
+#' @noRd
+print.ffep_download <- function(x, ...) {
+  cli::cli_alert("<ffexpectedpoints predictions>")
+  cli::cli_alert("Generated {.val {attr(x,'ep_timestamp')}} with ep model version {.val {attr(x,'ep_version')}}")
+  NextMethod(print,x)
+  invisible(x)
 }
